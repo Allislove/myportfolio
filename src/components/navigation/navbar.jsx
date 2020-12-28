43eba5f16c2ff7703ee53a178b14a5df33de762c
Allislove <xandrewssx970@outlook.es>
2020-12-28 14:50:30 -0500

    Updated v2.1

--- src/components/navigation/navbar.jsx
+++ src/components/navigation/navbar.jsx
@@ -6,17 +6,20 @@
 
 function Navbar() {
     return(
-            <nav className="menuown">
-                <div> <li><a href="#">Programador Web Colombia</a></li></div>
-                {/*<div>*/}
-                {/*    Logo aqui*/}
-                {/*</div>*/}
-                <div>
-                    <li><a href="#sobremi">Sobre mi</a></li>
-                    <li><a href="#contactame">Contacto</a></li>
-                    <li><a href="#mis-servicios" >Servicios</a> </li>
-                    <li><a href="#miportafolio" >Portafolio</a> </li>
+            <nav className="menuown navbar navbar-expand-lg">
+                <div className='container-fluid' >
+                    <div> <li><a href="#sobremi">Programador Web Colombia</a></li></div>
+                    {/*<div>*/}
+                    {/*    Logo aqui*/}
+                    {/*</div>*/}
+                    <div>
+                        <li><a href="#sobremi">Sobre mi</a></li>
+                        <li><a href="#contactame">Contacto</a></li>
+                        <li><a href="#mis-servicios" >Servicios</a> </li>
+                        <li><a href="#miportafolio" >Portafolio</a> </li>
+                    </div>
                 </div>
+
             </nav>
 
 
