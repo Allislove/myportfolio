5be87b97bb52d90077b0dca56b285f75c1c9112e
iamOther <iamother@evoluciondigital.com.co>
2021-11-30 12:22:52 -0500

    if user click on BLM logo redirects to home

--- src/components/views/helpers/blacklm.js
+++ src/components/views/helpers/blacklm.js
@@ -3,10 +3,15 @@ import './css/blacklm.css'
 
 // Black lives matter helper component
 export default function Blacklm() {
+
+  const handleClick = () => {
+    window.location.href = "/";
+  }
+  
   return (
-    <div className="blacklm">
+    <div className="blacklm" onClick={handleClick}>
       <div className="blacklm__text text-center">
-        <h1>Black Lives Matter</h1>
+        <h1 onClick={handleClick}>Black Lives Matter</h1>
       </div>
     </div>
   );
