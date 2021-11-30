21ff6ff3b23d0d913891b4a4444e80b7d77dc1b8
iamOther <iamother@evoluciondigital.com.co>
2021-11-30 16:36:43 -0500

    Added mobile menu

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import Navbar from './components/navigation/navbar';
 import Footer from './components/views/footer/footer';
 import './App.css';
+import Mobilemenu from './components/navigation/mobilemenu';
 
 function App() {
   return (
     <div className="container-sm">
+      <Mobilemenu />
       <Navbar />
       <Footer />
     </div>
