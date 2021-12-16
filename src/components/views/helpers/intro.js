cdcf68b88dc5e346c6d96101b0c111dbc914ceb4
iamOther <iamother@evoluciondigital.com.co>
2021-12-16 10:41:19 -0500

    Added width and height

--- src/components/views/helpers/intro.js
+++ src/components/views/helpers/intro.js
@@ -3,20 +3,25 @@ import Profileimage from "./images/profile.jpeg";
 import LatestPosts from "./latestposts";
 
 export default function Intro() {
+  return (
+    <div className="container mt-5">
+      <div className="text-center">
+        <a href="https://twitter.com/_iamar__" target="_blank" rel="noopener noreferrer">
+          <img
+            src={Profileimage}
+            width="200"
+            height="400"
+            className="rounded img-fluid"
+            alt="Es una foto de Andrés Romaña"
+          />
+        </a>
+        <div className="ITSME"> A F R R</div>
+      </div>
 
-    return(
-        <div className="container mt-5"> 
-            <div className="text-center">
-                <img src={Profileimage} className="rounded img-fluid" alt="javascript js" />
-                <div className="ITSME"> A F R R</div>
-            </div>
-
-            <h1>Latest Posts</h1>
-            <section className="intro-latest-posts">
-                <LatestPosts />    
-            </section>        
-        </div>
-    )
+      <h1>Latest Posts</h1>
+      <section className="intro-latest-posts">
+        <LatestPosts />
+      </section>
+    </div>
+  );
 }
-
-
