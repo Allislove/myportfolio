bfeaefcac6cd02250a529bc3a2fbed49576b5078
iamother <xandrewssx970@outlook.es>
2023-03-12 11:22:54 -0500

    finished

--- src/components/navigation/userIsNotLogged.js
+++ src/components/navigation/userIsNotLogged.js
@@ -7,7 +7,7 @@ import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
 const navigation = [
   { name: 'Home', href: '/', current: true },
   { name: 'Blog', href: '/blog', current: false },
-  { name: 'Portfolio', href: '/portfolio', current: false },
+  // { name: 'Portfolio', href: '/portfolio', current: false },
   { name: 'Aboutme', href: '/aboutme', current: false },
 ]
 
@@ -53,7 +53,7 @@ export default function UserIsNotLogged() {
                         key={item.name}
                         href={item.href}
                         className={classNames(
-                          item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                          item.current ? 'bg-[gray-900] text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
                           'rounded-md px-3 py-2 text-sm font-medium'
                         )}
                         aria-current={item.current ? 'page' : undefined}
@@ -150,34 +150,3 @@ export default function UserIsNotLogged() {
     </Disclosure>
   )
 }
-
-
-
-/* const UserIsNotLogged = () => {
-  return (
-    <nav className="flex fixed w-full inset-x-0 top-0 bg-[black] flex-row items-center justify-center p-1" id="">
-      <ul className="flex  p-2 [&>*]:font-bold [&>*]:rounded-md [&>*]:p-1 [&>*]:text-gray-200 [&>*]:mx-2 [&>*]:my-3 [&>*:hover]:text-red-500">
-        <li className="border-x-amber-500  rounded-full">
-          <Link to="/">Home</Link>
-        </li>
-        <li className="">
-          <Link className="a" to="/blog">
-            Blog
-          </Link>
-        </li>
-        <li className="" style={{visibility: "hidden"}}>
-          <Link to="/portfolio">Portfolio</Link>
-        </li>
-        <li className="">
-          <Link to="/aboutme">Aboutme</Link>
-        </li>
-        <li className="">
-          <Link to="/signin">Signin</Link>
-        </li>
-      </ul>
-    </nav>
-  );
-};
-
-export default UserIsNotLogged;
- */
