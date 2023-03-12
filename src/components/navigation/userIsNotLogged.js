fc117125cf240b3dcdb3a3ec3b01a413e701a995
iamother <xandrewssx970@outlook.es>
2023-03-12 00:22:50 -0500

    fix  finally

--- src/components/navigation/userIsNotLogged.js
+++ src/components/navigation/userIsNotLogged.js
@@ -67,14 +67,6 @@ export default function UserIsNotLogged() {
                 </div>
               </div>
               <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-                <button
-                  type="button"
-                  className="rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
-                >
-                  <span className="sr-only">View notifications</span>
-                  <BellIcon className="h-6 w-6" aria-hidden="true" />
-                </button>
-
                 {/* Profile dropdown */}
                 <Menu as="div" className="relative ml-3">
                   <div>
@@ -82,7 +74,7 @@ export default function UserIsNotLogged() {
                       <span className="sr-only">Open user menu</span>
                       <img
                         className="h-8 w-8 rounded-full"
-                        src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                        src="https://img.icons8.com/ios-glyphs/512/cat-profile--v1.png"
                         alt=""
                       />
                     </Menu.Button>
